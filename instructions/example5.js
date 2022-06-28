f963706d72fbf925b87e1dbb17e04c1e506d8826
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-06-28 09:37:35 +0400

    Update example5.js

--- instructions/example5.js
+++ instructions/example5.js
@@ -11,7 +11,10 @@ prompt.get('input', function (err, result) {
     var input = result.input; // "5,2 4 7 12 9"
     var input_data = input.split(','); // ['5', '2 4 7 12 9']
     var n = parseInt(input_data[0]); // Massivin element sayı. Nümunə - 5
-    var arr = input_data[1].split(' '); // Daxil edilmiş massiv. Nümunə - ['2', '4', '7', '12', '9']
-    arr.reverse();
-    console.log(arr);
+    var massiv = input_data[1].split(' '); // Daxil edilmiş massiv. Nümunə - ['2', '4', '7', '12', '9']
+    var sum = 0;
+    for(i=0;i<n;i++){
+        sum+=parseInt(massiv[i])
+    }
+    console.log(sum); // 34
 });
