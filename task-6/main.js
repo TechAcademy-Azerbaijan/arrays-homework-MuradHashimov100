64b98c8a7f6fdb775021529d6d7374111291477e
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-01 11:27:04 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -11,41 +11,24 @@ prompt.get('input', function (err, result) {
     var input = result.input; // "5,2 4 7 12 9"
     var input_data = input.split(','); // ['5', '2 4 7 12 9']
     var n = parseInt(input_data[0]); // Massivin element sayı. Nümunə - 5
-    var arr = input_data[1].split(' '); // Daxil edilmiş massiv. Nümunə - ['2', '4', '7', '12', '9']
-    let max = 0;
+    var arr = input_data[1].split(', '); // Daxil edilmiş massiv. Nümunə - ['2', '4', '7', '12', '9']
+    let max = arr[0];
+    let min  = arr[0]
     let sum = 0;
-    let min = arr[0];
     for(let i = 0;i<arr.length;i++){
-        if(arr[i]>max){
-            max=arr[i];
-            
-        }
-    }
-    
-    // console.log(max);
-    for(let i = 0;i<arr.length;i++){
-        if(arr[i]==max){
-             arr.splice(i,1);
-        }
-    }
-    
-    console.log(arr);
-    for(let i =0; i<arr.length;i++){
+      if(arr[i]>max){
+        max = arr[i];
         
-        if(arr[i]<min){
-            min = arr[i];
-        }
-    }   
-    // console.log(min);
-    for(let i = 0;i<arr.length;i++){
-        if(arr[i]==min){
-            arr.splice(i,i);
-        }
+      }
+      else if(arr[i]<min){
+        min = arr[i];
+      }
     }
-    console.log(arr);
+    console.log(max,min);
     for(let i = 0;i<arr.length;i++){
-        sum = sum + arr[i];
+     if(arr[i]!= max && arr[i]!=min){
+        sum = sum+arr[i];
+      }
     }
     console.log(sum);
-  
 });
